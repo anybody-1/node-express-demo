40a20f29810ed8d561bd2223ce2aacaf24455747
lvshuangyong <1721446288@qq.com>
2020-07-03 10:35:14 +0800

    修改为ts

--- app1.ts
+++
@@ -1,9 +0,0 @@
-// var express = require("express");
-import express from "express";
-var app = express();
-app.get("/", function (req, res) {
-  res.send("成功了");
-});
-app.listen(3000, function () {
-  console.log("Example app listening on port 3000!");
-});
